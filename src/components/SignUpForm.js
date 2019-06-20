3a4931909c450a7510a153570167854a3f741d39
Bohdan Kokovych <kokovych@gmail.com>
2019-06-21 00:25:29 +0300

    template of sign up form

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class SignUpForm extends Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render (){
+        return (
+            <form action="">
+                <h2 className="">Registration form:</h2>
+            </form>
+        );
+    }
+
+}
+
+export default SignUpForm;
