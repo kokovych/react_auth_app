333a7e6f79559c818fc79df1f7fb489ba5d2d4f7
Bohdan Kokovych <kokovych@gmail.com>
2019-06-23 01:31:06 +0300

    add primitive registration form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -3,13 +3,58 @@ import React, { Component } from "react";
 class SignUpForm extends Component{
 
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
     }
 
     render (){
         return (
-            <form action="">
+            <form onSubmit={this.onSubmit}>
                 <h2 className="">Registration form:</h2>
+                <div className="form-group">
+                    <label className="control-label">Email</label>
+                    <input
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        type="text"
+                        name="email"
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Password</label>
+                    <input
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        type="password"
+                        name="password"
+                        className="form-control"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <button className="btn btn-primary btn-lg">
+                        Sign up
+                    </button>
+                </div>
+
             </form>
         );
     }
